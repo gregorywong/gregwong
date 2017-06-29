08f3d450dc6d413d844664b9aa6a7ce055916954
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-06-29 16:07:41 +0800

    Linked to Medium

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $('.nav-link').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+// to prevent external linking navbar links from changing colors after being clicked
+$('.nav-link').on("focus", function() {
+  $(this).blur();
+});
